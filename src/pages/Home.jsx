8cdb8378f19af871b5e5e8065c31776340d2bb12
Banku-7971 <bmgoopop@gmail.com>
2026-08-05 03:19:04 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedProducts from '../components/FeaturedProducts'
+import Skeleton3D from '../components/Skeleton3D'
 import Newsletter from '../components/Newsletter'
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
     <div className="bg-noir">
       <HeroSection />
       <FeaturedProducts />
+      <Skeleton3D />
       <Newsletter variant="default" />
     </div>
   )
